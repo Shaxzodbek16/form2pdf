7a8dfdb9e875e67fc203badb616ee09ff051a531
Shaxzodbek <134439368+Shaxzodbek16@users.noreply.github.com>
2025-06-10 01:51:29 +0500

    Add application form and PDF generation

--- form2pdf/urls.py
+++ form2pdf/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path("form_data/", lambda x: x, name="form_data"),
-    path("download/", lambda x: x, name="download"),
+    path('form_data/', views.get_form_data, name='form_data'),
+    path('download/', views.download, name='download'),
 ]
